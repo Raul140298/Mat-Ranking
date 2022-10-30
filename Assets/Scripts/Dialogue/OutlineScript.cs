5583974bc6d54f17e091f12b576198a0242ab56f
Raul Jeri <raul.jeri@pucp.edu.pe>
2022-10-29 19:54:55 -0500

    Corrección de animación outline

--- Assets/Scripts/Dialogue/OutlineScript.cs
+++ Assets/Scripts/Dialogue/OutlineScript.cs
@@ -3,19 +3,29 @@ using UnityEngine;
 public class OutlineScript : MonoBehaviour
 {
     Animator animator;
+    Material material;
 
     private void Start()
     {
         animator = this.GetComponent<Animator>();
+        material = this.GetComponent<SpriteRenderer>().material;
     }
 
     public void OutlineOn()
     {
-        animator.SetBool("Outline", true);
+        if(this.gameObject.name == "Tower Entry") animator.SetBool("Outline", true);
+        else
+        {
+			material.SetFloat("_OutlineAlpha", 1f);
+		}
 	}
 
 	public void OutlineOff()
 	{
-		animator.SetBool("Outline", false);
+		if (this.gameObject.name == "Tower Entry") animator.SetBool("Outline", false);
+		else
+		{
+			material.SetFloat("_OutlineAlpha", 0f);
+		}
 	}
 }
