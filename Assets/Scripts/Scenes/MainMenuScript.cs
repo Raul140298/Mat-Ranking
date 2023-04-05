51dc3ae91a06a832d9eec2a137960581fceebcc1
Raul Jeri <raul.jeri@pucp.edu.pe>
2023-04-04 21:09:28 -0500

    [Feature] Add main menu background

--- Assets/Scripts/Scenes/MainMenuScript.cs
+++ Assets/Scripts/Scenes/MainMenuScript.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 public class MainMenuScript : SceneScript
 {
     [SerializeField] private Text version;
+    [SerializeField] private RawImage background;
+    [SerializeField] private float velocity;
 
     private void Start()
     {
@@ -14,5 +16,10 @@ public class MainMenuScript : SceneScript
 
         version.text = Application.version;
         SoundtracksScript.PlaySoundtrack("GARDEN OF MATH");
+    }
+
+    private void Update()
+    {
+        background.uvRect = new Rect(background.uvRect.position + new Vector2(velocity, velocity) * Time.deltaTime, background.uvRect.size);
     }
 }
